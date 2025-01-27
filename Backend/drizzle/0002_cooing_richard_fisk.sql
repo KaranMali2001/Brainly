87f4ecede8126a87d5cce86c8976ecceb6220476
karan <karanmali122001@gmail.com>
2025-01-28 00:44:47 +0530

    init 2

---
+++ Backend/drizzle/0002_cooing_richard_fisk.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" ALTER COLUMN "height" SET DEFAULT 180;
--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,3 +2,6 @@ import express from "express";
 import cors from "cors";
 export const app = express();
 app.use(cors({ origin: "*", credentials: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
